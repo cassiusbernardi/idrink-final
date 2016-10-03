755d15c8698eb8ead1d3952b9e2d44ae6ee95777
Cassius Bernardi <cassiusbernardi89@gmail.com>
2016-10-03 13:33:17 -0300

    initial commit

--- db/migrate/20160929175045_remove_nascimento_from_useres.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNascimentoFromUseres < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :useres, :nascimento, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160929175133) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categorias", force: :cascade do |t|
     t.string   "descricao"
     t.datetime "created_at", null: false
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160929175133) do
     t.datetime "updated_at",   null: false
     t.integer  "categoria_id"
     t.string   "img"
-    t.index ["categoria_id"], name: "index_produtos_on_categoria_id"
+    t.index ["categoria_id"], name: "index_produtos_on_categoria_id", using: :btree
   end
 
   create_table "useres", force: :cascade do |t|
@@ -58,8 +61,9 @@ ActiveRecord::Schema.define(version: 20160929175133) do
     t.string   "endereco"
     t.string   "telefone"
     t.date     "nascimento"
-    t.index ["email"], name: "index_useres_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_useres_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_useres_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_useres_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "produtos", "categorias"
 end
